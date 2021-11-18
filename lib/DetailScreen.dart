b221ed91766dfddd2a1661d48a3f58aecf20e6e7
Theejuth <86812783+Theejuth@users.noreply.github.com>
2021-11-18 14:37:03 +0700

    Update DetailScreen.dart

--- lib/DetailScreen.dart
+++ lib/DetailScreen.dart
@@ -1,75 +1,124 @@
+import 'package:firztair_by_theejutha/achievement_data.dart';
 import 'package:firztair_by_theejutha/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBlueColor,
-      appBar: AppBar(
-        backgroundColor: titleTextColor,
-        elevation: 0,
-        leading: IconButton(
-            padding: EdgeInsets.only(left: kDefaultPadding),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.arrow_back_ios_sharp,
-              color: Colors.black,
-            )),
-        centerTitle: false,
-        title: Text(
-          'Back'.toUpperCase(),
-          style: Theme.of(context).textTheme.bodyText2,
-        ),
-      ),
-      body: Body(),
-    );
-  }
-}
-
-class Body extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-          decoration: BoxDecoration(
-              color: titleTextColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
-              )),
+        body: SafeArea(
+      bottom: false,
+      child: Stack(children: <Widget>[
+        SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
-                height: size.width * 0.8,
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Container(
-                      height: size.width * 0.7,
-                      width: size.width * 0.7,
-                      decoration: BoxDecoration(
-                          color: Colors.white, shape: BoxShape.circle),
+                    SizedBox(
+                      height: 300,
                     ),
-                    Image.asset(
-                      'C:\Users\Acer\Desktop\FirztAir by Theejutha\firztair_by_theejutha\assets\images\icons\5855613.png',
-                      fit: BoxFit.cover,
+                    Text(
+                      "Injury Wound",
+                      style: TextStyle(
+                        fontSize: 56,
+                        color: kTextColor,
+                        fontWeight: FontWeight.w900,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    Divider(
+                      color: Colors.black38,
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Text(
+                      "injuries that break the skin or other body tissues. They include cuts, scrapes, scratches, and punctured skin. They often happen because of an accident, but surgery, sutures, and stitches also cause wounds.",
+                      maxLines: 8,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: primaryTextColor,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Divider(
+                      color: Colors.black38,
                     )
                   ],
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 32),
+                child: Text(
+                  "Gallery",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: kTextColor,
+                      fontWeight: FontWeight.w300),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Container(
+                height: 250,
+                child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.network(
+                              'https://images.onhealth.com/images/slideshow/caring-for-wounds-s1-blood-finger.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ));
+                    }),
               )
             ],
           ),
-        )
-      ],
-    );
+        ),
+        Positioned(
+            right: 8,
+            top: 20,
+            child: Image.network(
+              "https://www.shareicon.net/data/512x512/2015/10/27/662591_medical_512x512.png",
+              scale: 2,
+            )),
+        Positioned(
+            top: 60,
+            left: 32,
+            child: Text(
+              "1",
+              style: TextStyle(
+                  fontSize: 247,
+                  color: Colors.grey.withOpacity(0.1),
+                  fontWeight: FontWeight.w900),
+              textAlign: TextAlign.left,
+            )),
+        IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back_ios))
+      ]),
+    ));
   }
 }
