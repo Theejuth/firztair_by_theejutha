be501cd611f5a63cf818d34d5f0a24d1079ab72c
Theejuth <86812783+Theejuth@users.noreply.github.com>
2021-12-24 18:51:21 +0700

    Create User_preferences.dart

---
+++ lib/User_preferences.dart
@@ -0,0 +1,13 @@
+import 'User.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://www.seekpng.com/png/full/847-8474751_download-empty-profile.png',
+    name: 'Theejutha Konsombat',
+    email: '37975@virtual.prc.ac.th',
+    about:
+        'Certified Personal Trainer and Nutritionist with years of experience in creating effective diets and training plans focused on achieving individual customers goals in a smooth way.',
+    isDarkMode: false,
+  );
+}
